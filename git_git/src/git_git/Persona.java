f26f835b9e79ef076b22e5cb2db7e65a837025eb
joanieto@gmail.com <joanieto@gmail.com>
2017-03-14 21:16:34 +0100

    inicio

---
+++ git_git/src/git_git/Persona.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git_git;
+
+/**
+ *
+ * @author Sobremesa
+ */
+public class Persona {
+     private String nom;
+        private int edat;
+        
+         public Persona (String nom, int edat){
+             this.nom=nom;
+             this.edat=edat;
+         }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getEdat() {
+        return edat;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setEdat(int edat) {
+        this.edat = edat;
+    }
+         
+         
+}
